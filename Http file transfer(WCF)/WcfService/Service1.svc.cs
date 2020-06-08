cd6d0d78d755a5e31d0bfb52c350d767f7ec40ad
Lemon Lee <2010048424@qq.com>
2020-06-08 11:35:49 +0800

    文件传输操作

---
+++ Http file transfer(WCF)/WcfService/Service1.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace WcfService
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“Service1”。
+    // 注意: 为了启动 WCF 测试客户端以测试此服务，请在解决方案资源管理器中选择 Service1.svc 或 Service1.svc.cs，然后开始调试。
+    public class Service1 : IService1
+    {
+        public static List<string> filesInfo;
+        public static string path;
+        public List<string> GetFilesInfo()
+        {
+            if (filesInfo == null)
+            {
+                filesInfo = new List<string>();
+                path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DownloadFiles");
+                DirectoryInfo dir = new DirectoryInfo(path);
+                FileInfo[] fi = dir.GetFiles();
+                foreach (var i in fi)
+                {
+                    filesInfo.Add(string.Format("{0},{1}", i.Name, i.Length));
+                }
+            }
+
+            return filesInfo;
+        }
+
+        public Stream GetDownloadsStream(string FileName)
+        {
+            string filePath = Path.Combine(path, FileName);
+            try
+            {
+                FileStream imageFile = File.OpenRead(filePath);
+                return imageFile;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Http file transfer(WCF)/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.IO;
+using WpfApp1.ServiceReference1;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        string[] filesInfo;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Service1Client client = new Service1Client();
+
+            filesInfo = client.GetFilesInfo();
+            for (int i = 0; i < filesInfo.Length; i++)
+            {
+                string[] s = filesInfo[i].Split(',');
+                this.listBox.Items.Add(string.Format("文件名：{0}，文件长度：{1}字节", s[0], s[1]));
+            }
+            client.Close();
+            if (listBox.Items.Count > 0)
+            {
+                listBox.SelectedIndex = 0;
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Service1Client client = new Service1Client();
+            string[] s = filesInfo[listBox.SelectedIndex].Split(',');
+            this.txt.Text = "正在下载" + s[0];
+            string filePath = System.IO.Path.Combine(System.Environment.CurrentDirectory, s[0]);
+            Stream stream = client.GetDownloadsStream(s[0]);
+            DownloadFile(stream, filePath);
+        }
+
+        private void DownloadFile(Stream stream, string filePath)
+        {
+            FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+            const int bufferLen = 5000;
+            byte[] buffer = new byte[bufferLen];
+            int count = 0;
+            int byteCount = 0;
+            while ((count = stream.Read(buffer, 0, bufferLen)) > 0)
+            {
+                fs.Write(buffer, 0, count);
+                byteCount += count;
+            }
+            fs.Close();
+            stream.Close();
+            this.txt.Text = "\n下载完成，共下载" + byteCount + "字节，文件保存到" + filePath;
+        }
+    }
+}
